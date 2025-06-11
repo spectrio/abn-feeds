77e8c5ff6bfdc53169ca5774cb402608608e3594
Satbir Singh <satbir.singh@spectrio.com>
2025-06-11 20:24:03 +0530

    Updated News API URL

--- aggregator/config/digichief.php
+++ aggregator/config/digichief.php
@@ -5,7 +5,7 @@
  * 
  */
 return [
-	'xmlAllNewsEndpoint' => 'http://silo.digichief.com/ABN-Live-2-1-2012/NewsII/XML/AllStories.xml',
+	'xmlAllNewsEndpoint' => 'https://api.digichief.com/newsv2/getNews/Spectrio/bcb5e2fe-e898-4e98-9cc3-34900dc2a8b7?category=all&format=xml',
 	'xmlThisDateHistoryEndpoint' => 'http://silo.digichief.com/ABN-Live-2-1-2012/Almanac/XML/ThisDate.xml',
 	'xmlBornDateEndpoint' => 'http://silo.digichief.com/ABN-Live-2-1-2012/Almanac/XML/BornDate.xml',
 	
